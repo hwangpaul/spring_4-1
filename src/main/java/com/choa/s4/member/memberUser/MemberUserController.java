04c1c08691e1eb7b2954c1034c70fd89c11f9c91
hwangpaul <71800750+hwangpaul@users.noreply.github.com>
2020-11-04 12:50:44 +0900

    commit

--- src/main/java/com/choa/s4/member/memberUser/MemberUserController.java
+++ src/main/java/com/choa/s4/member/memberUser/MemberUserController.java
@@ -1,5 +1,6 @@
 package com.choa.s4.member.memberUser;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -130,8 +131,17 @@ public class MemberUserController {
 	}
 	
 	@PostMapping("memberLogin")
-	public ModelAndView getMemberLogin(MemberDTO memberDTO, HttpSession session)throws Exception{
+	public ModelAndView getMemberLogin(MemberDTO memberDTO, String remember , HttpSession session)throws Exception{
 		ModelAndView mv = new ModelAndView();
+		System.out.println("Remember : "+remember);
+		
+		//remember의 값이 null이 아니라면 cookie 발행
+		//cookie의 이름 remember, 밸류는 로그인 시 ID
+		if(remember != null) {
+			Cookie cookie = new Cookie("remember", memberDTO.getId());
+			
+		}
+		
 		System.out.println(memberDTO.getId());
 		System.out.println(memberDTO.getPw());
 		memberDTO = memberUserService.getMemberLogin(memberDTO);
